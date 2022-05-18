19419ce4e3f49097f88b3ed6c0a8ff86437ab3a0
Dmitry Shvetsov <ebashunabayane@gmail.com>
2022-05-18 18:55:14 +0700

    changed value radiobutton

--- claim/models.py
+++ claim/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Claim(models.Model):
-    type_claim = models.IntegerField()
+    type_claim = models.CharField(max_length=1024)
     select_claim = models.CharField(max_length=1024)
     name_claim = models.CharField(max_length=1024)
     parent_claim = models.CharField(max_length=1024)
