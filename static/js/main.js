b9f01cea791bd36c94268cc19419d6bedd29a14e
qbr <qbr95@vk.com>
2022-06-20 23:57:08 +0700

    update JS

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,36 @@
 function OpenModalWindow() {
-	document.getElementsByClassName('modal-window')[0].classList.add('modal--active');
-	document.getElementsByTagName("body")[0].style = "overflow: hidden;"
+	document.querySelector('.modal-window').classList.add('modal--active');
+	document.querySelector('body').style = "overflow: hidden;"
 }
 function CloseModalWindow() {
-	document.getElementsByClassName('modal-window')[0].classList.remove('modal--active');
-	document.getElementsByTagName("body")[0].style = ""
+	document.querySelector('.modal-window').classList.remove('modal--active');
+	document.querySelector('body').style = ""
 }
+
+const realFileBtn = document.querySelector('.input-fileUpload');
+const customBtn = document.querySelector('.button-fileUpload');
+const customText = document.querySelector('.fileUpload-text');
+
+customBtn.addEventListener('click', function () {
+	realFileBtn.click();
+})
+
+realFileBtn.addEventListener('change', function () {
+	var validExts = new Array(".xlsx", ".xls");
+	var fileExt = realFileBtn.value;
+	fileExt = fileExt.substring(fileExt.lastIndexOf('.'));
+	if (validExts.indexOf(fileExt) < 0) {
+		customText.innerHTML = 'Ошибка! Файл должен быть ' + validExts.toString();
+		return false;
+	} else {
+		if (realFileBtn.value) {
+			customText.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+		} else {
+			customText.innerHTML = 'Файл еще не выбран.'
+		}
+		return true;
+	}
+})
+
+
+
